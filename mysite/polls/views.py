91db21a5c14459374293bbc9e7296ce9e33792bd
skA <sskenner@gmail.com>
2019-12-27 11:59:27 -0500

    update views; add HttpResponse & template; update/add render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,27 +1,27 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
+
 # Create your views here.
-# def index(request):
-#   return HttpResponse("sup, world. ur at the polls index.")
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
   context = {
     'latest_question_list': latest_question_list,
   }
-  # output = ', '.join([q.question_text for q in latest_question_list])
-  return HttpResponse(template.render(context, request))
+  return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
   return HttpResponse("ur looking at quesion %s." % question_id)
 
+
 def results(request, question_id):
   response = "ur looking at the results of question %s."
   return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
   return HttpResponse("ur voting on question %s." % question_id)
