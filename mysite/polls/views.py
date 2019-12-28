42985b7928f177652a91eac99ace420d425a881a
sk <sskenner@gmail.com>
2019-12-28 11:48:33 -0500

    add 404 error(s); 2.5.4 Raising a 404 error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,5 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -13,15 +12,13 @@ def index(request):
   }
   return render(request, 'polls/index.html', context)
 
-
 def detail(request, question_id):
-  return HttpResponse("ur looking at quesion %s." % question_id)
-
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   response = "ur looking at the results of question %s."
   return HttpResponse(response % question_id)
-
 
 def vote(request, question_id):
   return HttpResponse("ur voting on question %s." % question_id)
