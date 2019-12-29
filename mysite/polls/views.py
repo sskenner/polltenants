ffedf8ccce93a4306fe840e23158d61da9a50cad
sk <sskenner@gmail.com>
2019-12-28 19:29:07 -0500

    add was_published_recently test; 2.7.3 Writing our first test

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -15,28 +15,15 @@ class IndexView(generic.ListView):
     """Return the last five published questions."""
     return Question.objects.order_by('-pub_date')[:5]
 
-# def index(request):
-#   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#   context = {
-#     'latest_question_list': latest_question_list,
-#   }
-#   return render(request, 'polls/index.html', context)
-
 class DetailView(generic.DetailView):
   model = Question
   template_name = 'polls/detail.html'
 
-# def detail(request, question_id):
-#   question = get_object_or_404(Question, pk=question_id)
-#   return render(request, 'polls/detail.html', {'question': question})
 
 class ResultsView(generic.DetailView):
   model = Question
   template_name = 'polls/results.html'
 
-# def results(request, question_id):
-#   question = get_object_or_404(Question, pk=question_id)
-#   return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
